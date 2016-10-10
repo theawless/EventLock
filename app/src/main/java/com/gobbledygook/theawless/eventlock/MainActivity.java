571236536c600b8e4b4eabc1a6f06721bd95e9a1
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-10-10 18:13:02 +0530

    Added warnings for xposed

--- app/src/main/java/com/gobbledygook/theawless/eventlock/MainActivity.java
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/MainActivity.java
@@ -1,8 +1,13 @@
 package com.gobbledygook.theawless.eventlock;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (savedInstanceState == null) {
+            if (!XposedUtils.isModuleEnabled()) {
+                showEnableModuleDialog();
+            } else if (XposedUtils.isModuleUpdated()) {
+                showModuleUpdatedDialog();
+            }
+        }
+    }
 
+    private void startXposedActivity(String section) {
+        if (!XposedUtils.startXposedActivity(this, section)) {
+            Toast.makeText(this, R.string.xposed_not_installed, Toast.LENGTH_SHORT).show();
+            startBrowserActivity("http://forum.xda-developers.com/showthread.php?t=3034811");
+        }
+    }
+
+    private void startBrowserActivity(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+
+    private void showEnableModuleDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.enable_xposed_module_title)
+                .setMessage(R.string.enable_xposed_module_message)
+                .setIcon(R.drawable.warning_icon)
+                .setPositiveButton(R.string.enable, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startXposedActivity(XposedUtils.XPOSED_SECTION_MODULES);
+                    }
+                })
+                .setNeutralButton(R.string.report_bug, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startBrowserActivity("https://github.com/theawless/EventLock/issues");
+                    }
+                })
+                .setNegativeButton(R.string.ignore, null)
+                .show();
+    }
+
+    private void showModuleUpdatedDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.module_outdated_title)
+                .setMessage(R.string.module_outdated_message)
+                .setIcon(R.drawable.warning_icon)
+                .setPositiveButton(R.string.reboot, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startXposedActivity(XposedUtils.XPOSED_SECTION_INSTALL);
+                    }
+                })
+                .setNegativeButton(R.string.ignore, null)
+                .show();
     }
 }
---
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/XposedUtils.java
@@ -0,0 +1,37 @@
+package com.gobbledygook.theawless.eventlock;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+
+public final class XposedUtils {
+    public static final String XPOSED_SECTION_MODULES = "modules";
+    public static final String XPOSED_SECTION_INSTALL = "install";
+    private static final int MODULE_VERSION = BuildConfig.MODULE_VERSION;
+    private static final String XPOSED_PACKAGE = "de.robv.android.xposed.installer";
+    private static final String XPOSED_ACTION = XPOSED_PACKAGE + ".OPEN_SECTION";
+    private static final String XPOSED_EXTRA_SECTION = "section";
+
+    public static boolean isModuleEnabled() {
+        return getModuleVersion() >= 0;
+    }
+
+    public static boolean isModuleUpdated() {
+        return MODULE_VERSION != getModuleVersion();
+    }
+
+    private static int getModuleVersion() {
+        return -1;
+    }
+
+    public static boolean startXposedActivity(Context context, String section) {
+        Intent intent = new Intent(XPOSED_ACTION);
+        intent.putExtra(XPOSED_EXTRA_SECTION, section);
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (ActivityNotFoundException e) {
+            return false;
+        }
+    }
+}
