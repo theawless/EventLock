f916e65ebe383ba31d0837da9d2f672d6b73168d
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-10-10 20:32:34 +0530

    Bug Fix : load default values on start

--- app/src/main/java/com/gobbledygook/theawless/eventlock/MainActivity.java
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
         if (savedInstanceState == null) {
             if (!XposedUtils.isModuleEnabled()) {
                 showEnableModuleDialog();
