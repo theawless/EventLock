8a0705e2d3081e4dc31858c279e090e732147b14
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-10-11 20:20:57 +0530

    Added keys to static class and updated string resources

---
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/PreferenceConsts.java
@@ -0,0 +1,17 @@
+package com.gobbledygook.theawless.eventlock;
+
+
+public class PreferenceConsts {
+    public final static String preferences = "event_lock_prefs";
+    public final static String authority = preferences + ".preferences";
+    public final static String selected_calendars_key = "selected_calendars_preference_key";
+    public final static String free_key = "free_preference_key";
+    public final static String from_key = "from_preference_key";
+    public final static String to_key = "to_preference_key";
+    public final static String max_events_key = "max_events_preference_key";
+    public final static String time_font_key = "time_font_preference_key";
+    public final static String title_font_key = "title_font_preference_key";
+
+    private PreferenceConsts() {
+    }
+}
