fbad7c5d701c915744c067b68dc4da061d866242
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-10-15 01:51:52 +0530

    Bug fix: update at midnight

--- app/src/main/java/com/gobbledygook/theawless/eventlock/SchedulingService.java
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/SchedulingService.java
@@ -18,12 +18,16 @@ import android.util.Log;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SchedulingService extends IntentService {
     private static final String TAG = "SchedulingService";
     private static final String[] EVENT_PROJECTION = {CalendarContract.Instances.TITLE, CalendarContract.Instances.EVENT_LOCATION, CalendarContract.Instances.BEGIN, CalendarContract.Instances.END, CalendarContract.Instances.ALL_DAY};
-    private static final String SELECTION = CalendarContract.Instances.CALENDAR_ID + " = ?";
     private AlarmReceiver alarmReceiver = new AlarmReceiver();
+    private Context context;
+    private SharedPreferences preferences;
 
     public SchedulingService() {
         super(TAG);
@@ -38,33 +42,44 @@ public class SchedulingService extends IntentService {
 
     protected void handleIntent(Context context) {
         Log.v(TAG, "handleIntent");
-        SharedPreferences preferences = context.getSharedPreferences(PreferenceConsts.preferences, MODE_PRIVATE);
+        preferences = context.getSharedPreferences(context.getString(R.string.preferences), MODE_PRIVATE);
+        this.context = context;
+        if (!checkPermissions()) {
+            return;
+        }
+        String[] default_selected_calendars_array = context.getResources().getStringArray(R.array.selected_calendars_default);
+        Set<String> selected_calendars_set_default = new HashSet<>();
+        Collections.addAll(selected_calendars_set_default, default_selected_calendars_array);
+        Set<String> selectionArgsSet = preferences.getStringSet(context.getString(R.string.selected_calendars_key), selected_calendars_set_default);
+        if (selectionArgsSet.isEmpty()) {
+            //no calendars set, quit the service
+            setTitleAndTimePrefs("", "");
+            stopSelf();
+        }
+        queryCalendarEvents(buildSelection(selectionArgsSet.size()), selectionArgsSet.toArray(new String[selectionArgsSet.size()]));
+    }
+
+    private void queryCalendarEvents(String selection, String[] selectionArgs) {
         Calendar calendar = Calendar.getInstance();
         long from_time = calendar.getTimeInMillis();
-        if (!preferences.getBoolean(PreferenceConsts.from_key, Boolean.parseBoolean(PreferenceConsts.from_default))) {
+        if (!preferences.getBoolean(context.getString(R.string.from_key), Boolean.parseBoolean(context.getString(R.string.from_default)))) {
             calendar.set(Calendar.SECOND, 1);
             calendar.set(Calendar.MINUTE, 0);
             calendar.set(Calendar.HOUR_OF_DAY, 0);
             from_time = calendar.getTimeInMillis();
         }
-        int to_key_value = Integer.parseInt(preferences.getString(PreferenceConsts.to_key, PreferenceConsts.to_default));
+        int to_key_value = Integer.parseInt(preferences.getString(context.getString(R.string.to_key), context.getString(R.string.to_default)));
         calendar.set(Calendar.SECOND, 59);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.HOUR_OF_DAY, 23);
+        long tonight_time = calendar.getTimeInMillis();
         calendar.add(Calendar.DATE, to_key_value);
         long to_time = calendar.getTimeInMillis();
-        SharedPreferences.Editor editor = preferences.edit();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context.checkSelfPermission(Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            Log.v(TAG, "No READ_CALENDAR permission");
-            editor.remove(PreferenceConsts.event_title_key);
-            editor.remove(PreferenceConsts.event_time_key);
-            return;
-        }
         Uri.Builder eventsUriBuilder = CalendarContract.Instances.CONTENT_URI.buildUpon();
         ContentUris.appendId(eventsUriBuilder, from_time);
         ContentUris.appendId(eventsUriBuilder, to_time);
-        String[] selectionArgs = new String[]{preferences.getString(PreferenceConsts.selected_calendars_key, PreferenceConsts.selected_calendars_default)};
-        Cursor cursor = context.getContentResolver().query(eventsUriBuilder.build(), EVENT_PROJECTION, SELECTION, selectionArgs, CalendarContract.Instances.BEGIN + " ASC");
+        String finalTime = "", finalTitle = "";
+        Cursor cursor = context.getContentResolver().query(eventsUriBuilder.build(), EVENT_PROJECTION, selection, selectionArgs, CalendarContract.Instances.BEGIN + " ASC");
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
             String eventTitle = cursor.getString(0);
@@ -73,12 +88,12 @@ public class SchedulingService extends IntentService {
             long endTime = cursor.getLong(3);
             int allDay = cursor.getInt(4);
             cursor.close();
-            String finalTitle = eventTitle;
+            finalTitle = eventTitle;
             if (!TextUtils.isEmpty(eventLocation)) {
                 finalTitle = finalTitle + " " + context.getString(R.string.at) + " " + eventLocation;
             }
             DateFormat formatter = SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT);
-            String finalTime = formatter.format(beginTime) + " - " + formatter.format(endTime);
+            finalTime = formatter.format(beginTime) + " - " + formatter.format(endTime);
             if (allDay == 1) {
                 finalTime = context.getString(R.string.all_day);
             }
@@ -103,20 +118,45 @@ public class SchedulingService extends IntentService {
                     finalTime = finalTime + ", " + context.getString(R.string.after) + " " + (day_diff - 1) + " " + context.getString(R.string.days);
                 }
             }
-            editor.putString(PreferenceConsts.event_title_key, finalTitle);
-            editor.putString(PreferenceConsts.event_time_key, finalTime);
-            editor.apply();
             //event ends at endTime, let's check back at endTime + 2 second
-            alarmReceiver.setAlarm(context, endTime + 1000 * 2);
-            return;
-        }
-        if (cursor != null) {
+            if (endTime < tonight_time) {
+                //compulsary check at each midnight
+                tonight_time = endTime;
+            }
+        } else if (cursor != null) {
             cursor.close();
         }
-        editor.putString(PreferenceConsts.event_title_key, "");
-        editor.putString(PreferenceConsts.event_time_key, "");
+        //no events in the upcoming days, let's check back at midnight + 2 second
+        setTitleAndTimePrefs(finalTitle, finalTime);
+        alarmReceiver.setAlarm(context, tonight_time + 1000 * 2);
+    }
+
+    private void setTitleAndTimePrefs(String title, String time) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(context.getString(R.string.event_title_key), title);
+        editor.putString(context.getString(R.string.event_time_key), time);
         editor.apply();
-        //no events in the upcoming days, let's check back at toTime + 2 second
-        alarmReceiver.setAlarm(context, to_time + 1000 * 2);
+    }
+
+
+    private String buildSelection(int length) {
+        String selection = "( " + CalendarContract.Instances.CALENDAR_ID + " = ?";
+        for (int i = 0; i < length - 1; i++) {
+            selection = selection + " OR " + CalendarContract.Instances.CALENDAR_ID + " = ?";
+        }
+        selection = selection + " )";
+        return selection;
+    }
+
+    private boolean checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context.checkSelfPermission(Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "No READ_CALENDAR permission");
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.remove(context.getString(R.string.event_title_key));
+            editor.remove(context.getString(R.string.event_time_key));
+            editor.apply();
+            return false;
+        }
+        return true;
     }
 }
