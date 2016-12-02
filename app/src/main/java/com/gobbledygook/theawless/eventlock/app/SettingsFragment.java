fe6f03b69efd21907568548041906b8a9a9e963d
Abhinav Singh <theawless@gmail.com>
2016-12-02 12:20:28 +0530

    Better settings

--- app/src/main/java/com/gobbledygook/theawless/eventlock/app/SettingsFragment.java
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/app/SettingsFragment.java
@@ -2,8 +2,6 @@ package com.gobbledygook.theawless.eventlock.app;
 
 import android.Manifest;
 import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -12,50 +10,97 @@ import android.os.Bundle;
 import android.preference.MultiSelectListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.provider.CalendarContract;
-import android.util.Log;
+import android.support.annotation.NonNull;
 import android.widget.Toast;
 
 import com.gobbledygook.theawless.eventlock.R;
-import com.gobbledygook.theawless.eventlock.background.CalendarLoaderService;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragment {
     private static final int CALENDAR_READ_REQUEST_CODE = 0;
-    private static final String TAG = "SETTINGS_FRAGMENT";
+    private final static int nonEmptyPreferences[] = new int[]{
+            R.string.padding_above_key, R.string.padding_below_key, R.string.padding_left_key, R.string.padding_right_key,
+            R.string.title_padding_above_key, R.string.title_padding_below_key, R.string.title_padding_left_key, R.string.title_padding_right_key,
+            R.string.time_padding_above_key, R.string.time_padding_below_key, R.string.time_padding_left_key, R.string.time_padding_right_key,
+            R.string.color_padding_above_key, R.string.color_padding_below_key, R.string.color_padding_left_key, R.string.color_padding_right_key,
+            R.string.color_height_key, R.string.color_width_key, R.string.color_radius_key,
+            R.string.days_till_key, R.string.number_of_events_key,
+    };
     private int versionClickCount = 0;
+    //stored in weakhash map; hence must store them to prevent garbage collection
+    private ArrayList<Preference.OnPreferenceChangeListener> preferenceListeners = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getPreferenceManager().setSharedPreferencesName(getString(R.string.preferences));
-        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         addPreferencesFromResource(R.xml.preferences);
-        checkAndRequestPermission();
+        setUpPreferenceCleaners();
+        setUpConditionalPreferences();
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        Intent intent = new Intent(getActivity(), CalendarLoaderService.class);
-        getActivity().startService(intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && getActivity().checkSelfPermission(Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, CALENDAR_READ_REQUEST_CODE);
+        } else {
+            createCalendarList();
+        }
     }
 
     @Override
-    public void onDestroy() {
-        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-        super.onDestroy();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CALENDAR_READ_REQUEST_CODE) {
+            if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), R.string.read_permission_error, Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            } else {
+                createCalendarList();
+            }
+        }
+    }
+
+    private void setUpPreferenceCleaners() {
+        for (int keyId : nonEmptyPreferences) {
+            Preference preference = getPreferenceManager().findPreference(getString(keyId));
+            Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    return !newValue.toString().trim().isEmpty();
+                }
+            };
+            preference.setOnPreferenceChangeListener(listener);
+            preferenceListeners.add(listener);
+        }
+    }
+
+    private void setUpConditionalPreferences() {
+        final PreferenceManager preferenceManager = getPreferenceManager();
+        boolean ovalSet = preferenceManager.getSharedPreferences().getString(getString(R.string.color_type_key), getString(R.string.color_type_default)).equals(getResources().getStringArray(R.array.color_type_entvals)[0]);
+        preferenceManager.findPreference(getString(R.string.color_height_key)).setEnabled(!ovalSet);
+        preferenceManager.findPreference(getString(R.string.color_width_key)).setEnabled(!ovalSet);
+        preferenceManager.findPreference(getString(R.string.color_radius_key)).setEnabled(ovalSet);
+        preferenceManager.findPreference(getString(R.string.color_type_key)).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                boolean ovalSet = newValue.toString().equals(getResources().getStringArray(R.array.color_type_entvals)[0]);
+                preferenceManager.findPreference(getString(R.string.color_height_key)).setEnabled(!ovalSet);
+                preferenceManager.findPreference(getString(R.string.color_width_key)).setEnabled(!ovalSet);
+                preferenceManager.findPreference(getString(R.string.color_radius_key)).setEnabled(ovalSet);
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (!preference.getTitle().equals(getString(R.string.version_title))) {
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
-        } else {
-            ++versionClickCount;
-            switch (versionClickCount) {
+        if (preference.getTitleRes() == R.string.version_title) {
+            switch (++versionClickCount) {
                 case 1: {
                     Toast.makeText(getActivity(), R.string.version_count_three, Toast.LENGTH_SHORT).show();
                     break;
@@ -75,64 +120,26 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
             }
             return true;
         }
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals(getString(R.string.days_till_key))) {
-            String keyVal = sharedPreferences.getString(key, "");
-            if (keyVal.isEmpty()) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(key, "0");
-                editor.apply();
-            }
-        }
-        if (key.equals(getString(R.string.selected_calendars_key)) || key.equals(getString(R.string.days_till_key))) {
-            Log.v(TAG, "Staring service");
-            Intent intent = new Intent(getActivity(), CalendarLoaderService.class);
-            getActivity().startService(intent);
-        }
-    }
-
-    void checkAndRequestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && getActivity().checkSelfPermission(Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CALENDAR}, CALENDAR_READ_REQUEST_CODE);
-        } else {
-            createMultiSelectCalendars();
-        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     @SuppressWarnings("MissingPermission")
-    private void createMultiSelectCalendars() {
+    public void createCalendarList() {
         ContentResolver resolver = getActivity().getContentResolver();
         Uri calendarUri = CalendarContract.Calendars.CONTENT_URI;
         String[] projection = new String[]{CalendarContract.Calendars._ID, CalendarContract.Calendars.CALENDAR_DISPLAY_NAME};
         Cursor cursor = resolver.query(calendarUri, projection, null, null, null);
-        final ArrayList<String> calendarNames = new ArrayList<>();
-        final ArrayList<String> calendarIds = new ArrayList<>();
-        if (cursor != null) {
-            while (cursor.moveToNext()) {
-                calendarNames.add(cursor.getString(1));
-                calendarIds.add(Integer.valueOf(cursor.getInt(0)).toString());
-            }
-            cursor.close();
+        List<String> calendarNames = new ArrayList<>();
+        List<String> calendarIds = new ArrayList<>();
+        while (cursor != null && cursor.moveToNext()) {
+            calendarNames.add(cursor.getString(1));
+            calendarIds.add(Integer.valueOf(cursor.getInt(0)).toString());
         }
-        MultiSelectListPreference selectedCalendarPref = (MultiSelectListPreference) findPreference(getString(R.string.selected_calendars_key));
-        selectedCalendarPref.setEntries(calendarNames.toArray(new CharSequence[calendarNames.size()]));
-        selectedCalendarPref.setEntryValues(calendarIds.toArray(new CharSequence[calendarNames.size()]));
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == CALENDAR_READ_REQUEST_CODE) {
-            if (grantResults.length <= 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getActivity(), R.string.read_permission_error, Toast.LENGTH_SHORT).show();
-                getActivity().finish();
-            } else {
-                createMultiSelectCalendars();
-            }
-        } else {
-            Log.wtf(TAG, "Weird request code!");
+        if (cursor != null) {
+            cursor.close();
+            MultiSelectListPreference selectedCalendarPref = (MultiSelectListPreference) findPreference(getString(R.string.selected_calendars_key));
+            selectedCalendarPref.setEntries(calendarNames.toArray(new CharSequence[calendarNames.size()]));
+            selectedCalendarPref.setEntryValues(calendarIds.toArray(new CharSequence[calendarNames.size()]));
         }
     }
 }
