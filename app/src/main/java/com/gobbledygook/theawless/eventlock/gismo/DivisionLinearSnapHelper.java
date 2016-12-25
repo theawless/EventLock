6c112e2f5cb13542d62bba28b3c594d6c09c8671
Abhinav Singh <theawless@gmail.com>
2016-12-25 21:49:11 +0530

    Bug Fix: Snappy snapper

--- app/src/main/java/com/gobbledygook/theawless/eventlock/gismo/DivisionLinearSnapHelper.java
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/gismo/DivisionLinearSnapHelper.java
@@ -7,8 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 class DivisionLinearSnapHelper extends LinearSnapHelper {
-    private boolean[] positiveVelocity = new boolean[2];
     private int divisionFactor;
+    private int targetIndex;
 
     void setDivisionFactor(int divisionFactor) {
         this.divisionFactor = divisionFactor;
@@ -16,9 +16,7 @@ class DivisionLinearSnapHelper extends LinearSnapHelper {
 
     @Override
     public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager, int velocityX, int velocityY) {
-        positiveVelocity[0] = velocityX > 0;
-        positiveVelocity[1] = velocityY > 0;
-        return super.findTargetSnapPosition(layoutManager, velocityX, velocityY);
+        return targetIndex = super.findTargetSnapPosition(layoutManager, velocityX, velocityY);
     }
 
     @Override
@@ -26,9 +24,9 @@ class DivisionLinearSnapHelper extends LinearSnapHelper {
         int distance[] = super.calculateDistanceToFinalSnap(layoutManager, targetView);
         if (divisionFactor % 2 == 0 && distance != null) {
             if (((GridLayoutManager) layoutManager).getOrientation() == GridLayoutManager.VERTICAL) {
-                distance[1] += (positiveVelocity[1]) ? (targetView.getHeight() / 2) : -(targetView.getHeight() / 2);
+                distance[1] += targetIndex == 0 ? -targetView.getHeight() / 2 : targetView.getHeight() / 2;
             } else {
-                distance[0] += (positiveVelocity[0]) ? (targetView.getWidth() / 2) : -(targetView.getWidth() / 2);
+                distance[0] += targetIndex == 0 ? -targetView.getWidth() / 2 : targetView.getWidth() / 2;
             }
         }
         return distance;
