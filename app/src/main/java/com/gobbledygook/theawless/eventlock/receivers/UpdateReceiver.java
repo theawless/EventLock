theawless/EventLock

commit 201057a65166c0708e9e68bef966313448848130

Fix for lint/performance warnings.

app/src/main/java/com/gobbledygook/theawless/eventlock/receivers/UpdateReceiver.java
@@ -18,6 +18,9 @@ public abstract class UpdateReceiver extends BroadcastReceiver {
             return;
         }
         Bundle bundle = intent.getExtras();
+        if (bundle == null) {
+            return;
+        }
         switch (action) {
             case Constants.events_update:
                 eventsGismo.deliverNewEvents(

app/build.gradle
@@ -10,7 +10,6 @@ android {
         versionName "3.0"
         testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
-        buildConfigField("int", "MODULE_VERSION", "20")
     }
     buildTypes {
         release {

app/src/main/java/com/gobbledygook/theawless/eventlock/app/CalendarPreference.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 
 class CalendarPreference extends MultiSelectListPreference {
-
     public CalendarPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }

app/src/main/java/com/gobbledygook/theawless/eventlock/app/MainActivity.java
@@ -25,7 +25,7 @@ import com.gobbledygook.theawless.eventlock.services.CalendarLoaderService;
 public class MainActivity extends AppCompatActivity {
     private boolean previewOn = false;
     private EventsGismo eventGismo = null;
-    private UpdateReceiver updateReceiver = new UpdateReceiver() {
+    private final UpdateReceiver updateReceiver = new UpdateReceiver() {
         @Override
         protected EventsGismo getGismo(String action) {
             return eventGismo;
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void handlePreview(MenuItem item) {
         if (eventGismo == null) {
-            GridLayout gridLayout = (GridLayout) findViewById(R.id.events_placeholder);
+            GridLayout gridLayout = findViewById(R.id.events_placeholder);
             eventGismo = new EventsGismo(gridLayout, PreferenceManager.getDefaultSharedPreferences(this));
             eventGismo.addRecyclerView();
             handleRefresh();

app/src/main/java/com/gobbledygook/theawless/eventlock/app/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.gobbledygook.theawless.eventlock.app;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -32,9 +33,8 @@ public class SettingsFragment extends PreferenceFragment implements SettingsRefr
             R.string.color_height_key, R.string.color_width_key, R.string.days_till_key,
             R.string.number_of_events_key,
     };
-    //reasons to fucking hate java
-    //no initialisers for map or even arraylist wtf bro
-    //shitty syntax
+
+    //reasons to hate java - shitty syntax
     private static final ArrayList<Integer> versionClicks = new ArrayList<>(Arrays.asList(1, 6, 12, 18, 25, 30, 40, 50, 60, 70, 80, 90, 100));
     private static final int versionStrings[] = new int[]{
             R.string.version_count_1, R.string.version_count_6, R.string.version_count_12,
@@ -43,16 +43,16 @@ public class SettingsFragment extends PreferenceFragment implements SettingsRefr
             R.string.version_count_70, R.string.version_count_80, R.string.version_count_90,
             R.string.version_count_100,
     };
-    private int versionClickCount = 0;
-    //stored in weakhash map; hence must store it to prevent garbage collection
-    private Preference.OnPreferenceChangeListener preferenceListener = new Preference.OnPreferenceChangeListener() {
+    private final Preference.OnPreferenceChangeListener preferenceListener = new Preference.OnPreferenceChangeListener() {
         @Override
         public boolean onPreferenceChange(Preference preference, Object newValue) {
             return !newValue.toString().trim().isEmpty();
         }
     };
+    private int versionClickCount = 0;
     private PresetMaker presetMaker;
 
+    @SuppressLint("WorldReadableFiles")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -103,7 +103,9 @@ public class SettingsFragment extends PreferenceFragment implements SettingsRefr
     private void setUpPreferenceCleaners() {
         for (int keyId : nonEmptyPreferences) {
             Preference preference = getPreferenceManager().findPreference(getString(keyId));
-            preference.setOnPreferenceChangeListener(preferenceListener);
+            if (preference != null) {
+                preference.setOnPreferenceChangeListener(preferenceListener);
+            }
         }
     }
 

app/src/main/java/com/gobbledygook/theawless/eventlock/app/VersionPreference.java
@@ -1,0 +1,14 @@
+package com.gobbledygook.theawless.eventlock.app;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+
+import com.gobbledygook.theawless.eventlock.BuildConfig;
+
+public class VersionPreference extends Preference {
+    public VersionPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setSummary(BuildConfig.VERSION_NAME);
+    }
+}

app/src/main/java/com/gobbledygook/theawless/eventlock/events/EventsBuildDirector.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class EventsBuildDirector {
     private static final String[] EVENT_PROJECTION = {CalendarContract.Instances.TITLE, CalendarContract.Instances.EVENT_LOCATION, CalendarContract.Instances.BEGIN, CalendarContract.Instances.END, CalendarContract.Instances.ALL_DAY, CalendarContract.Instances.DISPLAY_COLOR, CalendarContract.Instances.EVENT_TIMEZONE};
     private final SharedPreferences preferences;
-    private EventsBuilder eventsBuilder;
+    private final EventsBuilder eventsBuilder;
 
     public EventsBuildDirector(Context context) {
         eventsBuilder = new EventsBuilder(context);
@@ -40,11 +40,11 @@ public class EventsBuildDirector {
     }
 
     private String buildSelection(int length) {
-        String selection = "( " + CalendarContract.Instances.CALENDAR_ID + " = ?";
+        StringBuilder selection = new StringBuilder("( " + CalendarContract.Instances.CALENDAR_ID + " = ?");
         for (int i = 0; i < length - 1; i++) {
-            selection = selection + " OR " + CalendarContract.Instances.CALENDAR_ID + " = ?";
+            selection.append(" OR " + CalendarContract.Instances.CALENDAR_ID + " = ?");
         }
-        selection = selection + " )";
-        return selection;
+        selection.append(" )");
+        return selection.toString();
     }
 }

commit 427b4b26393779385abda9e5cafdb1686c16b341

Bugfix: Looks update was not working
Missed the call to gismo notify preferences. This bug was introduced in
commit 201057a65166c0708e9e68bef966313448848130.

app/src/main/java/com/gobbledygook/theawless/eventlock/receivers/UpdateReceiver.java
@@ -18,19 +18,20 @@ public abstract class UpdateReceiver extends BroadcastReceiver {
             return;
         }
         Bundle bundle = intent.getExtras();
-        if (bundle == null) {
-            return;
-        }
         switch (action) {
             case Constants.events_update:
-                eventsGismo.deliverNewEvents(
-                        bundle.getStringArrayList(Constants.formatted_titles),
-                        bundle.getStringArrayList(Constants.formatted_times),
-                        bundle.getStringArrayList(Constants.colors)
-                );
+                if (bundle != null) {
+                    eventsGismo.deliverNewEvents(
+                            bundle.getStringArrayList(Constants.formatted_titles),
+                            bundle.getStringArrayList(Constants.formatted_times),
+                            bundle.getStringArrayList(Constants.colors)
+                    );
+                }
                 break;
             case Constants.current_event_update:
-                eventsGismo.deliverNewCurrentEvent(bundle.getInt(Constants.current_event));
+                if (bundle != null) {
+                    eventsGismo.deliverNewCurrentEvent(bundle.getInt(Constants.current_event));
+                }
                 break;
             case Constants.looks_update:
                 eventsGismo.notifyUpdatedPreferences();