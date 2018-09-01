22a83820666aa2cbc1e5c1e7dc75d23efbd5ebf4
theawless <theawless@gmail.com>
2018-09-01 18:14:52 +0530

    Show version code

--- app/src/main/java/com/gobbledygook/theawless/eventlock/app/VersionPreference.java
+++ app/src/main/java/com/gobbledygook/theawless/eventlock/app/VersionPreference.java
@@ -9,6 +9,6 @@ import com.gobbledygook.theawless.eventlock.BuildConfig;
 public class VersionPreference extends Preference {
     public VersionPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setSummary(BuildConfig.VERSION_NAME);
+        setSummary(BuildConfig.VERSION_NAME + '.' + BuildConfig.VERSION_CODE);
     }
 }
